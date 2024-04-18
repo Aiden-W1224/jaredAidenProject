c930e06af86adf61210ac7be5bece64f30725121
Aiden-W1224 <aidenwong40@gmail.com>
2024-04-18 23:17:36 +0900

    renaming file

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello world'),
+      body: JSON.stringify('Tokyo!!'),
     };
     return response;
   };
